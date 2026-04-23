e516ff8fae29c9ff688607920f1027db5820ee0b
Johnattan Manso <72331805+johnattanmanso@users.noreply.github.com>
2026-04-22 21:38:45 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,22 @@
+// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Dados oficiais que você pegou do painel
 const firebaseConfig = {
   apiKey: "AIzaSyCBgsFf6ynDU60_ITWQ1sNtYiCyKyyE83I",
   authDomain: "sistema-apostas-757f2.firebaseapp.com",
   projectId: "sistema-apostas-757f2",
-  storageBucket: "sistema-apostas-757f2.appspot.com",
+  storageBucket: "sistema-apostas-757f2.firebasestorage.app", // Atualizado
   messagingSenderId: "32782540371",
   appId: "1:32782540371:web:9a870595691aa9c3c5f0a6",
   measurementId: "G-91GW8B889B"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta para os outros arquivos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
